1f0d05d461cd8ef5caeac4cfab9ad9adc23f5a9a
vcc121 <vaughn.cox@bsu.edu>
2025-03-21 14:51:47 +0000

    adding to Customer

--- Customer.java
+++ Customer.java
@@ -2,8 +2,11 @@ import.java.util.*;
 import.java.io.*;
 
 public class Customer extends User {
-protected CheckingAccount checking;
-protected SavingsAccount savings;
+    protected CheckingAccount checking;
+    protected SavingsAccount savings;
 
-
+    public Customer(String userName, String PIN) {
+        this.checking = new CheckingAccount();
+        this.savings = new SavingsAccount();
+    }
 }
